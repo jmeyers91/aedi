bcd7d4247a0f556e226af9e5009775e25e4347dd
James Meyers <jamesmeyers919@gmail.com>
2023-09-22 23:24:34 -0700

    Add dropdown menu to test app.

--- apps/test-app/src/app/components/layout.tsx
+++ apps/test-app/src/app/components/layout.tsx
@@ -3,6 +3,10 @@ import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 import { blockButtonClassName } from './block-button';
 import { PlusIcon } from './icons/plus-icon';
+import { Menu, Transition } from '@headlessui/react';
+import { LogoutIcon } from './icons/logout-icon';
+import { DownloadIcon } from './icons/download-icon';
+import { MenuIcon } from './icons/menu-icon';
 
 export function Layout() {
   const { user, logout } = useContext(AuthContext);
@@ -13,22 +17,66 @@ export function Layout() {
         <Link className="text-3xl font-semibold text-sky-800" to="/">
           Contacts
         </Link>
-        <div className="flex flex-row items-center gap-8 pr-8">
-          <Link
-            to="/contacts/add"
-            className={`${blockButtonClassName()} flex items-center gap-2`}
-          >
-            <PlusIcon /> Add Contact
-          </Link>
-          {!!user && (
-            <button
-              onClick={logout}
-              className="text-gray-500 hover:text-gray-700 transition-colors"
+        {!!user && (
+          <div className="flex flex-row items-center gap-8 pr-8">
+            <Link
+              to="/contacts/add"
+              className={`${blockButtonClassName()} flex items-center gap-2`}
             >
-              Logout
-            </button>
-          )}
-        </div>
+              <PlusIcon /> Add Contact
+            </Link>
+
+            <Menu>
+              <div className="relative">
+                <Menu.Button>
+                  <button className="text-sky-500 hover:text-sky-800 transition-colors">
+                    <MenuIcon />
+                  </button>
+                </Menu.Button>
+                <Transition
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                  className="absolute right-0 z-10"
+                >
+                  <Menu.Items>
+                    <div className="w-60 p-8 flex flex-col items-start gap-6 text-gray-700 bg-white shadow-xl rounded">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={() => {
+                              console.log('TODO: Download contacts CSV');
+                            }}
+                            className={`w-full flex gap-4 ${
+                              active ? 'text-sky-800' : ''
+                            }`}
+                          >
+                            <DownloadIcon /> Export contacts
+                          </button>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={logout}
+                            className={`w-full flex gap-4 ${
+                              active ? 'text-red-600' : ''
+                            }`}
+                          >
+                            <LogoutIcon /> Logout
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </div>
+            </Menu>
+          </div>
+        )}
       </header>
       <Outlet />
     </div>
--- apps/test-app/src/app/pages/contacts-page/contact-list.tsx
+++ apps/test-app/src/app/pages/contacts-page/contact-list.tsx
@@ -2,8 +2,11 @@ import { formatContactName } from '../../utils/name-utils';
 import { Link } from 'react-router-dom';
 import type { Contact } from 'libs/test-cases/src/lib/static-site';
 import { SearchIcon } from '../../components/icons/search-icon';
+import { useState } from 'react';
 
 export function ContactList({ contacts }: { contacts: Contact[] }) {
+  const [search, setSearch] = useState('');
+
   return (
     <div className="relative flex flex-col gap-6 pb-16 pt-4 flex-1 h-full overflow-auto">
       <div className="flex flex-shrink-0 justify-between gap-6 sticky left-0 right-0 top-0 py-3 px-6">
@@ -11,6 +14,8 @@ export function ContactList({ contacts }: { contacts: Contact[] }) {
           <input
             placeholder="Search"
             className="w-full rounded-lg p-4 bg-gray-100 shadow-lg ring-inherit"
+            value={search}
+            onChange={(event) => setSearch(event.currentTarget.value)}
           />
           <SearchIcon className="pointer-events-none absolute right-4 top-3.5 text-emerald-800" />
         </div>
@@ -21,28 +26,40 @@ export function ContactList({ contacts }: { contacts: Contact[] }) {
             <p>You don't have any contacts yet. Add some.</p>
           </li>
         )}
-        {contacts.map((contact) => (
-          <li key={contact.contactId} className="flex">
-            <Link
-              to={`/contacts/view/${contact.contactId}`}
-              className="flex items-center gap-4 w-full hover:bg-gray-100 transition-colors px-6 py-4"
-            >
-              <img
-                src="profile-pic.png"
-                className="w-16 h-16 rounded-full overflow-hidden bg-center object-cover bg-gray-200"
-              />
+        {contacts
+          .filter((contact) => {
+            if (search.length === 0) {
+              return true;
+            }
+            return (
+              contact.firstName.includes(search) ||
+              contact.lastName.includes(search) ||
+              contact.email.includes(search) ||
+              contact.phone.includes(search)
+            );
+          })
+          .map((contact) => (
+            <li key={contact.contactId} className="flex">
+              <Link
+                to={`/contacts/view/${contact.contactId}`}
+                className="flex items-center gap-4 w-full hover:bg-gray-100 transition-colors px-6 py-4"
+              >
+                <img
+                  src="profile-pic.png"
+                  className="w-16 h-16 rounded-full overflow-hidden bg-center object-cover bg-gray-200"
+                />
 
-              <div className="flex flex-col gap-1">
-                <p className="font-semibold">
-                  {formatContactName(contact.firstName, contact.lastName)}
-                </p>
-                <p>
-                  {contact.email}, {contact.phone}
-                </p>
-              </div>
-            </Link>
-          </li>
-        ))}
+                <div className="flex flex-col gap-1">
+                  <p className="font-semibold">
+                    {formatContactName(contact.firstName, contact.lastName)}
+                  </p>
+                  <p>
+                    {contact.email}, {contact.phone}
+                  </p>
+                </div>
+              </Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
